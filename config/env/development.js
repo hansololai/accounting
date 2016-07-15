faa46ea0f222e3734779c187083f0b6db0e16ef9
Han Lai <han.lai321@gmail.com>
2016-07-15 14:17:33 -0400

    db setup

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
    models: {
-     connection: 'localDiskDb'
+     connection: 'sqlitedb'
    },
    sendFakeEmail:true,
    //port:80
